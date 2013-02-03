5002c1871054611cebe0f4ebf1bb78b6c6d1591b
ansh2201 <ansh2201@gmail.com>
2013-02-03 00:46:25 -0800

    Added logic for capture

---
+++ src/mancala.py
@@ -0,0 +1,79 @@
+'''
+Created on Feb 2, 2013
+
+@author: anduril
+'''
+#!/usr/bin/python
+
+# Head ends here
+
+'''
+Created on Feb 2, 2013
+
+@author: anduril
+'''
+#!/usr/bin/python
+
+# Head ends here
+
+def checkFreeTurn(board):
+    for i in range(len(board)):
+        if i == (len(board) - i): return i
+        else: return -1
+        
+def findRightmostSlot(board):
+    for i in range(len(board)):
+        if board[len(board)- 1 -i] != 0:
+            return len(board) - i
+        
+
+def capture(board, board_opp):
+    a = []
+    for i in range(len(board)):
+        if board[i] == 0: a.append(i)
+    pos=nos=0   #position of slot with 0, nos refers to number of marbles in the opposite slot.
+    for i in a:
+        for index in range(i):
+            if board[index] == (i - index):
+                if board_opp[len(board_opp)-1-i]:
+                    pos, nos = i, board_opp[len(board_opp)-1-i]
+    if nos != 0: return pos
+    else: return -1
+                     
+def chooseNextSlot(mancala, board, mancala_opp, board_opp):
+    #check if there exists a hole that leads to a free turn.
+    c = checkFreeTurn(board)    
+    a = capture(board, board_opp)
+    
+    if c != -1 : slot = c
+    elif a != -1: slot = a
+    else: slot = findRightmostSlot(board)
+    
+    return slot
+
+def printNextMove(player, player1Mancala, player1Marbles, player2Mancala, player2Marbles):
+    if player == 1 :
+        mancala, board = player1Mancala, player1Marbles
+        mancala_opp, board_opp = player2Mancala, player2Marbles
+    else :
+        mancala, board = player2Mancala, player2Marbles
+        mancala_opp, board_opp = player1Mancala, player1Marbles
+    
+    firstTurn = all(True if k == 4 else False for k in board)
+    firstTurn_opp = all(True if k == 4 else False for k in board_opp)
+    
+    if firstTurn and firstTurn_opp:
+        slot = 3
+    else:
+        slot = chooseNextSlot(mancala, board, mancala_opp, board_opp)
+    
+    return str(slot)
+
+
+# Tail starts here
+player = input()
+mancala1 = input()
+mancala1_marbles = [int(i) for i in raw_input().strip().split()]
+mancala2 = input()
+mancala2_marbles = [int(i) for i in raw_input().strip().split()]
+print printNextMove(player, mancala1, mancala1_marbles, mancala2, mancala2_marbles)
--- src/stockmarket.py
+++ src/stockmarket.py
@@ -1,13 +1,35 @@
-def print_transaction(money, _name, _owned, _prices):
-    
+import pickle
+
+def loadStockHistory(f):
+    try:
+        d = pickle.load(open(f,"rb"))
+    except:
+        d = {}
+    return d
+
+def writeStockHistory(f, d):
+    pickle.dump(d, open(f, "wb"))
+    return
+
+def print_transaction(_file, money, _name, _owned, _prices):
     decision = []
     output = []
     
-    for i in range(len(_name)):        
-        owned, prices = _owned[i], _prices[i]
+    historicalBuys = loadStockHistory(_file)
+    
+    for i in range(len(_name)):
+        historical_price = None
+        if historicalBuys.has_key(_name[i]):
+            historical_price = historicalBuys.get(_name[i])
+                    
+        owned, prices = _owned[i], _prices[i]        
         avg = avg_list(prices[:-1])
         
-        margin = prices[-1] - avg
+        if historical_price is not None:
+            margin = prices[-1] - (historical_price if historical_price > avg else avg)
+        else:
+            margin = prices[-1] - avg 
+        #margin = prices[-1] - avg
         decision.append((i, owned, margin))
         
     decision_sorted = sorted(decision, key=lambda tup: tup[2])
@@ -15,20 +37,26 @@ def print_transaction(money, _name, _owned, _prices):
     
     for i in decision_sorted:
         index, owned, margin = i
+        name = _name[index]
         if((-1)*margin > 0 and money > 0):
             last_price = _prices[index][-1]            
             units = int(money/last_price)                
             if units > 0: 
                 #print "appending"
                 money -= units * last_price
-                output.append((_name[index], "BUY", units))            
+                output.append((name, "BUY", units)) 
+                historicalBuys[name] = last_price #overwrite historical data.
+                            
         elif(margin > 0 and owned > 0):
             #print "appending"
-            output.append((_name[index], "SELL", owned))
+            output.append((name, "SELL", owned))
+            if historicalBuys.has_key(name): historicalBuys.pop(name)
     
     print len(output)
     for t in output:
         print_decision(t[0], t[1], t[2])
+    
+    writeStockHistory(_f, historicalBuys)
     return
 
 def print_decision(name, decision, units):
@@ -38,6 +66,7 @@ def avg_list(list):
     return reduce(lambda x, y: x + y, list) / len(list)
 
 if __name__ == '__main__':
+    _f = "murphy_stockpredictor.tmp"
     _in = raw_input().split()
     _m = float(_in[0])
     _k, _d = (int(i) for i in _in[1:])
@@ -48,5 +77,5 @@ if __name__ == '__main__':
         _name[i], _owned[i] = name, int(owned)
         _prices[i] = [float(j) for j in prices.split()]        
     
-    print_transaction(_m, _name, _owned, _prices)
+    print_transaction(_f, _m, _name, _owned, _prices)
     
